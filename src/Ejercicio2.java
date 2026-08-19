8a548ef5e953e00afc7cb9eba63ad430d961d931
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-08-19 12:49:37 -0500

    realizacion_de_ejercico2_saludo_estudiante

---
+++ src/Ejercicio2.java
@@ -0,0 +1,9 @@
+public class Ejercicio2 {
+    public static void main (String[] args){
+        saludarEstudiante( "angie", 12);
+
+    }
+    public static void saludarEstudiante(String nombre, int edad){
+       System.out.println("hola estudiante: " + nombre + " tu edad es: " + edad);
+    }
+}
