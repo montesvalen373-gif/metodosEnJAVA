f4ca2cb061f75d52c675e08a85f65b811dd311e0
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-08-19 14:10:34 -0500

    realizacion_de_ejercico4_numero_par

---
+++ src/Ejercicio4.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Ejercicio4 {
+    public static void main (String[] args){
+
+        System.out.println(esPar(100));
+    }
+    public static boolean esPar(int numero){
+        boolean clasificacion;
+        if (numero %2 == 0){
+            return (clasificacion) = true;
+        }else {
+            return (clasificacion) = false;
+        }
+
+    }
+}
