c5ebb1958a882cc507f067574da994c54e9ebd62
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-08-19 13:00:21 -0500

    realizacion_de_ejercico3_area_rectangulo

---
+++ src/Ejercicio3.java
@@ -0,0 +1,9 @@
+public class Ejercicio3 {
+    public static void main (String[] args){
+        double resultado = calcularAreaRectangulo(5, 3);
+        System.out.println(resultado);
+    }
+    public static double calcularAreaRectangulo(double base, double altura){
+        return base * altura;
+    }
+}
