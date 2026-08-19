17fab6bb78c917e0cee69ef753d706ed78a2e775
Angie Valentina Buitrago Montes <montesvalen373@gmail.com>
2026-08-19 12:30:58 -0500

    realizacion_de_ejercico1_bienvenido_instituto

---
+++ src/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main (String[] args){
+        mostrarBienvenida();
+    }
+    public static void mostrarBienvenida(){
+        System.out.println("bienvenido a nuestro instituto");
+    }
+}
